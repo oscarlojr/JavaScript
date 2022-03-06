513d22d05e73277577e36eb29d27966f781c7d4f
Oscar Lima Oliveira Junior <oscarlojr@gmail.com>
2022-03-06 06:47:48 -0300

    JS Ex1

---
+++ JavaScript_Inicio/ex001/1-MeuPrimeiroPrograma.js
@@ -0,0 +1,9 @@
+console.log("Meu primeiro Programa: trabalhando com variáveis");
+
+const idade = 37;
+
+console.log(idade); 
+console.log(idade+1); 
+
+const idadeSomada = idade+3;
+console.log(idadeSomada);
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# JavaScript
+# JavaScript Início
+
+ Relação de exercícios do curso de JavaScript. Diretório criado para fins didáticos.
  
