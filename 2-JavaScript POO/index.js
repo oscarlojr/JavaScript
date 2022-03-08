647d902e796ca0f0ffe654005e5369dc60b6a42f
Oscar Lima Oliveira Junior <oscarlojr@gmail.com>
2022-03-08 19:21:34 -0300

    Criando Metodos

--- 2-JavaScript POO/index.js
+++ 2-JavaScript POO/index.js
@@ -5,17 +5,20 @@ class Cliente{
 
 class ContaCorrente{
     agencia;
-    saldo;
+    #saldo = 0;
 
     sacar(valor){
-        if(this.saldo >= valor){
-            this.saldo -= valor;
+        if(this.#saldo >= valor){
+            this.#saldo -= valor;
+        }
+    }
+
+    depositar(valor){
+        if(valor > 0){
+            this.#saldo += valor;
         }
     }
 }
-
-
-
 
 const cliente1 = new Cliente();
 cliente1.nome = "Ricardo";
@@ -26,17 +29,9 @@ cliente2.nome = "Alice";
 cliente2.cpf = 88822233309;
 
 const contaCorrenteRicardo = new ContaCorrente();
-contaCorrenteRicardo.saldo = 0;
 contaCorrenteRicardo.agencia = 1001;
-console.log(contaCorrenteRicardo.saldo);
 
-contaCorrenteRicardo.saldo = 1000;
-console.log(contaCorrenteRicardo.saldo);
-
-
+contaCorrenteRicardo.depositar(100);
 contaCorrenteRicardo.sacar(50);
-console.log(contaCorrenteRicardo.saldo);
 
-
-console.log(cliente1);
-console.log(cliente2)
+console.log(contaCorrenteRicardo);
