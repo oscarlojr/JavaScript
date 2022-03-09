faeb53b8b6f5c36553813f1d7af7947ad267b2ac
Oscar Lima Oliveira Junior <oscarlojr@gmail.com>
2022-03-09 07:47:11 -0300

    Return e early return

--- 2-JavaScript POO/index.js
+++ 2-JavaScript POO/index.js
@@ -5,18 +5,19 @@ class Cliente{
 
 class ContaCorrente{
     agencia;
-    #saldo = 0;
+    _saldo = 0;
 
     sacar(valor){
-        if(this.#saldo >= valor){
-            this.#saldo -= valor;
+        if(this._saldo >= valor){
+            this._saldo -= valor;
         }
+    return valor;
     }
 
     depositar(valor){
-        if(valor > 0){
-            this.#saldo += valor;
-        }
+        if(valor <= 0) return;
+        
+        this._saldo += valor;
     }
 }
 
@@ -24,14 +25,11 @@ const cliente1 = new Cliente();
 cliente1.nome = "Ricardo";
 cliente1.cpf = 11122233309;
 
-const cliente2 = new Cliente();
-cliente2.nome = "Alice";
-cliente2.cpf = 88822233309;
-
 const contaCorrenteRicardo = new ContaCorrente();
 contaCorrenteRicardo.agencia = 1001;
+const valorDepositado = contaCorrenteRicardo.depositar(100);
+const valorSacado = contaCorrenteRicardo.sacar(50);
 
-contaCorrenteRicardo.depositar(100);
-contaCorrenteRicardo.sacar(50);
-
+console.log(`Este foi o valor sacado: ${valorSacado}`);
+console.log(`O valor depositado foi de: ${valorDepositado}`)
 console.log(contaCorrenteRicardo);
