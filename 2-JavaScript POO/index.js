55471fa9e38114c237a6a104ce928d4dfe5ebc2a
Oscar Lima Oliveira Junior <oscarlojr@gmail.com>
2022-03-15 10:51:27 -0300

    Encapsulamento
    *	Diferenças entre null e undefined;
    *	Aprofundando nos conceitos de referências a um espaço de memória;
    *	Get e Set;
    *	Melhor encapsulamento da classe;
    *	Protegendo atributos sensíveis;

--- 2-JavaScript POO/index.js
+++ 2-JavaScript POO/index.js
@@ -1,5 +1,5 @@
 import { Cliente } from "./Cliente.js";
-import{ContaCorrente} from "./ContaCorrente.js";
+import { ContaCorrente } from "./ContaCorrente.js";
 
 const cliente1 = new Cliente();
 cliente1.nome = "Ricardo";
@@ -10,15 +10,16 @@ cliente2.nome = "Alice";
 cliente2.cpf = 987654321;
 
 const conta1 = new ContaCorrente();
-conta1.agencia = 1001;
 conta1.cliente = cliente1;
+conta1.agencia = 1001;
 conta1.depositar(500);
 
 
 const conta2 = new ContaCorrente();
-conta2.agencia = 1001;
 conta2.cliente = cliente2;
+conta2.agencia = 1001;
 
-conta1.tansferir(200,conta2);
-console.log(conta1, conta2);
+
+conta1.tansferir(200, conta2);
+console.log(conta1, conta2.cliente);
 
