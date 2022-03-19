5de95618727e3e658ed443b336ca973df1392755
Oscar Lima Oliveira Junior <oscarlojr@gmail.com>
2022-03-19 15:58:16 -0300

    Criando estruturas de pasta

--- 2-JavaScript POO/index.js
+++ 2-JavaScript POO/index.js
@@ -1,8 +1,7 @@
 import { Cliente } from "./Cliente.js";
-import { Conta } from "./Conta.js";
-import { ContaCorrente } from "./ContaCorrente.js";
-import { ContaPoupanca } from "./ContaPoupanca.js";
-import { ContaSalario } from "./ContaSalario.js";
+import { ContaCorrente } from "./Contas/ContaCorrente.js";
+import { ContaPoupanca } from "./Contas/ContaPoupanca.js";
+import { ContaSalario } from "./Contas/ContaSalario.js";
 
 const cliente1 = new Cliente("Ricardo",11122233309);
 
