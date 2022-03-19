776bfaae0bb8c322dbf3fe859126c0425340ae97
Oscar Lima Oliveira Junior <oscarlojr@gmail.com>
2022-03-19 13:46:55 -0300

    Criação de classe abstrata

--- 2-JavaScript POO/Conta.js
+++ 2-JavaScript POO/Conta.js
@@ -1,12 +1,14 @@
+// Classe abstrata
 export class Conta{
     constructor(saldoInicial,cliente,agencia){
+        if(this.constructor == Conta ){
+            throw new Error ("Você não deveria instanciar um objeto do tipo conta diretamente.");
+        }
         this._saldo = saldoInicial;
         this._cliente = cliente;
         this._agencia = agencia;
 
-        if(this.constructor == Conta ){
-            console.log("Você não deveria instanciar um objeto do tipo conta.");
-        }
+       
     }
 
     set cliente(novoValor){
