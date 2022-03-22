61d085b83fe57759010f790f50c32b5d100d494d
Oscar Lima Oliveira Junior <oscarlojr@gmail.com>
2022-03-22 08:01:20 -0300

    Polimorfismo parte 2

--- 2-JavaScript POO/index.js
+++ 2-JavaScript POO/index.js
@@ -6,7 +6,12 @@ import { SistemaAutenticacao } from "./SistemaAutenticacao.js";
 const diretor = new Diretor("Rodrigo",10000,123456789);
 diretor.cadastrarSenha("123456");
 const gerente = new Gerente("Ricardo",5000,987654321);
+gerente.cadastrarSenha("654321");
 
-const estaLogado = SistemaAutenticacao.login(diretor,"123456");
+const cliente = new Cliente("Lais", 852741963,"456")
 
-console.log(estaLogado);
+const diretorEstaLogado = SistemaAutenticacao.login(diretor,"123456");
+const gerenteEstaLogado = SistemaAutenticacao.login(gerente,"654321");
+const clienteEstaLogado = SistemaAutenticacao.login(cliente,"456");
+
+console.log(diretorEstaLogado,gerenteEstaLogado,clienteEstaLogado);
